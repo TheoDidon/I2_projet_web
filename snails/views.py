6936d4047356f82dca711d90203a7d64c1ed0667
Theo Didon <theo.didon@gmail.com>
2023-11-11 13:40:22 +0100

    Equipements

--- snails/views.py
+++ snails/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Character
+from .models import Character, Equipement
 
 def post_list(request):
     characters = Character.objects.all()
-    return render(request, 'snails/post_list.html', {'characters': characters})
+    equipements = Equipement.objects.all()
+    return render(request, 'snails/post_list.html', {'characters': characters, 'equipements':equipements})
 
 
 def post_detail(request, pk):
     character = get_object_or_404(Character, pk=pk)
-    return render(request, 'snails/post_detail.html', {'character': character})
+    equipement = get_object_or_404(Equipement, pk=pk)
+    return render(request, 'snails/post_detail.html', {'character': character, 'equipement':equipement})
